42443a64e7e12772010f215f499109fcea0815b8
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-07 14:48:04 -0700

    error handling for fethcing IP

--- iss.js
+++ iss.js
@@ -9,12 +9,20 @@
 const request = require('request');
 
 const fetchMyIP = function(callback) {
-  request("https://api.ipify.org", (error, response, body) => {
+  request("https://api.ipify.org?format=json", (error, response, body) => {
     if (error) {
       callback(error, null);
-    } else {
-      callback(null, body);
     }
+    
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    
+    const data = JSON.parse(body);
+    callback(null, data.ip);
+
   });
 };
 
