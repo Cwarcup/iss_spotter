7884608ff94fe51e02dee84ee3489f56335d840d
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-07 16:32:15 -0700

    fetchISSFlyOverTimes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -12,7 +12,14 @@ fetchMyIP((error, ip) => {
       return;
     }
 
-    console.log(`It worked! Returned coordinates: ${JSON.stringify(coords)}`);
+    fetchISSFlyOverTimes(coords, (error, flyTimes) => {
+      if (error) {
+        console.log("It didn't work!" , error);
+        return;
+      }
+
+      console.log("It worked!" , flyTimes);
+    });
   });
 });
 
